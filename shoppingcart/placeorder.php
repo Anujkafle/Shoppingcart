b7518385165b65f7c30592b64eb154526a4d244c
Anujkafle <117552084+Anujkafle@users.noreply.github.com>
2023-02-27 14:38:32 +0545

    Add files via upload

---
+++ shoppingcart/placeorder.php
@@ -0,0 +1,9 @@
+<?=template_header('Place Order')?>
+
+<div class="placeorder content-wrapper">
+    <h1>Your Order Has Been Placed</h1>
+    <p>Thank you for ordering with us, we'll contact you by email with your order details.</p>
+    <p>For Further information Contact us in 9840255111</p>
+</div>
+
+<?=template_footer()?>
